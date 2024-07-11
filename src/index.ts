dab8cd3bd6dfdd0183250cb2d3ceec7d1709dd53
jishnu79 <jishnu@toolfe.com>
2024-07-11 17:00:20 +0530

    user post category

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
 import express from 'express'
+const {PrismaClient} =require('@prisma/client')
 
 const app = express()
+ 
+app.use(express.json()) 
 
-app.use(express.json())
+const prisma = new PrismaClient()
+
+async function main() {
+    const user = await prisma.user.findMany()
+    console.log(user);
+    
+}
+
+main()
+    .catch(e => {
+        console.log(e.message);
+    })
+    .finally(async () => {
+        await prisma.$disconnect()
+    })
 
 app.listen(3000, () => {
     console.log("Server running on port 3000");
